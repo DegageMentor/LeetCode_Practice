d4caf54c9a380160a0f1ae51cb313e030542a3b7
DegageMentor <maoxx0123@gmail.com>
2020-07-07 23:54:17 +0800

    62.不同路径,排列组合,动态规划

--- src/leetcode_practice/Main.java
+++ src/leetcode_practice/Main.java
@@ -2,8 +2,7 @@ package leetcode_practice;
 
 public class Main {
 	public static void main(String[] args) {
-		Object obj = new Question62();
-		
+		Object obj = new Question62();		
 		// 62.²»Í¬Â·¾¶
 		int m = 10, n = 10;
 		System.out.println(((Question62)obj).uniquePaths(m, n));
