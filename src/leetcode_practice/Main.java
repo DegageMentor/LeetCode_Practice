a4b327d6d193a2e100635f25ab76743718d0f054
DegageMentor <maoxx0123@gmail.com>
2020-07-08 22:44:39 +0800

    50.myPow（x,n）,快速幂,迭代

--- src/leetcode_practice/Main.java
+++ src/leetcode_practice/Main.java
@@ -2,10 +2,18 @@ package leetcode_practice;
 
 public class Main {
 	public static void main(String[] args) {
-		Object obj = new Question62();		
-		// 62.²»Í¬Â·¾¶
-		int m = 10, n = 10;
-		System.out.println(((Question62)obj).uniquePaths(m, n));
+		
+		Object obj = new Question50();
+		
+		//50.Pow(x,n)
+		double x = 2.0;
+		int n = 10;
+		System.out.println(((Question50)obj).myPow(x, n));
+		
+		// 62.ä¸åŒè·¯å¾„
+//		int m = 10, n = 10;
+//		System.out.println(((Question62)obj).uniquePaths(m, n));
+		
 	}
 
 }
