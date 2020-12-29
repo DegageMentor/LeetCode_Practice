b34e822f36b7f62744693a16cd9c47a305666d6e
DegageMentor <40751037+DegageMentor@users.noreply.github.com>
2020-12-29 18:31:55 +0800

    44，通配符匹配，动态规划

--- src/leetcode_practice/Main.java
+++ src/leetcode_practice/Main.java
@@ -7,12 +7,20 @@ public class Main {
 	// 用例测试
 	public static void main(String[] args) {
 
-		Object obj = new Question330();
+		Object obj = new Question44();
+		
+		//44.通配符匹配
+		String s1 = "aa";
+		String p1 = "a";
+		String s2 = "aa";
+		String p2 = "*";
+		System.out.println(((Question44)obj).isMatch(s1, p1));
+		System.out.println(((Question44)obj).isMatch(s2, p2));
 		
 		//330.按要求补齐数组
-		int[] nums = new int[] {1, 3};
-		int n = 6;
-		System.out.println(((Question330)obj).minPatches(nums, n));
+//		int[] nums = new int[] {1, 3};
+//		int n = 6;
+//		System.out.println(((Question330)obj).minPatches(nums, n));
 		
 		//135.分发糖果
 //		int[] ratings1 = new int[] {1, 0, 2};
